2a0176089840270bd1e31bcf168d27b88319aa61
Dafrom <daffaromero@gmail.com>
2026-01-02 20:03:19 +0700

    feat: migrated to HAML

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,6 @@ class ProductsController < ApplicationController
    private
 
    def product_params
-     params.require(:product).permit(:name, :description :price, :stock)
+    params.require(:product).permit(:name, :description, :price, :stock)
    end 
 end
